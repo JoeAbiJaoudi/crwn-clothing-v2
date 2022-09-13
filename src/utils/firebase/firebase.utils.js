3a5165224669a18bcabbb39d396a1fa5c968dfe5
JoeAbiJaoudi <joemunji25@gmail.com>
2022-09-13 12:26:30 -0400

    added sign in comp as well as firebase utils and brough in firebase libraies

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,9 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC5K9nRzYXLN8L0GRZGoaF1QGmVVSMCPGM",
@@ -18,14 +21,43 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-
 const provider = new GoogleAuthProvider(); //first step get a provider
 provider.setCustomParameters({
-
-    prompt : "select_account",
-
+  prompt: "select_account",
 }); //for Google Auth Provider
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
+//points to data base
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    //if the user snapshot does not exist we want to set it in our database
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log(`error creating the user`, error.message);
+    }
+  }
+
+  //if use data exists
+
+  //if user data does not exists
+
+  return userDocRef;
+};
